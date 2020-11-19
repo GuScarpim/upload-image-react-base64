1192d7880029f24617a0c3e893b24aafe6697413
guscarpim <gustavoscarpim@gmail.com>
2020-11-19 00:31:15 -0300

    Adicionando projeto typescript

---
+++ front/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Upload from './pages/Upload';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Upload />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
